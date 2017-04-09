31bfc12306c8b9108b968a15388f12f48d459259
Mark Cabanero <me@markcabanero.com>
2017-04-09 00:19:23 -0400

    Trying to establish client connection within functions.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Webservice that acts as the glue between Amazon and MongoDB.
 """
@@ -6,7 +8,6 @@ import pymongo
 import requests
 
 NYTIMES_API_KEY = None
-MONGODB_CONNECT = False
 CLIENT = None
 
 def initialize():
@@ -16,10 +17,13 @@ def initialize():
         with open('nytimes-key') as nytimes:
             NYTIMES_API_KEY = nytimes.read()
 
-    if not MONGODB_CONNECT:
+    if not CLIENT:
         with open('mongodb-key') as mongodb:
             CLIENT = pymongo.MongoClient(mongodb)
-            MONGODB_CONNECT = True
+            try:
+                CLIENT.admin.command('ismaster')
+            except pymongo.errors.ConnectionFailure:
+                print("Server is inaccessible.")
 
 
 def nytimes_critic_movies(type="reviews", resource_type="picks", offset="0", order="by-opening-date"):
@@ -48,10 +52,21 @@ def nytimes_search_movies(offset="0", order="by-opening-date", query=""):
     return r.json()
 
 
+def db_insert_one(entry):
+    if len(entry) != 1:
+        raise ValueError("Too many records trying to be put in.")
+    title = entry[0]["display_title"]
+    information = entry[0]
+    db = test.alexa
+    db.movies.count()
+    db.movies.insert_one(information)
+
+
 def main():
     initialize()
     nytimes_critic_movies()
-    nytimes_search_movies(query="28 days later")
+    db_insert_one(nytimes_search_movies(query="28 days later")["results"])
+    db_insert_one(nytimes_search_movies(query="toy story 3")["results"])
 
 
 if __name__ == '__main__':
