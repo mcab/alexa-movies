7dddd54e0eaeef2acebad16d34b2baf0d3cb6ddc
Mark Cabanero <me@markcabanero.com>
2017-04-08 20:44:52 -0400

    Do not include MongoDB connect string.

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 Webservice that acts as the glue between Amazon and MongoDB.
 """
 
+import pymongo
+
 NYTIMES_API_KEY = None
+MONGODB_CONNECT = False
+CLIENT = None
 
 def initialize():
-    global NYTIMES_API_KEY
+    global NYTIMES_API_KEY, MONGODB_CONNECT, CLIENT
+
     if not NYTIMES_API_KEY:
         with open('nytimes-key') as nytimes:
             NYTIMES_API_KEY = nytimes.read()
+
+    if not MONGODB_CONNECT:
+        with open('mongodb-key') as mongodb:
+            CLIENT = pymongo.MongoClient(mongodb)
+            MONGODB_CONNECT = True
+
 
 def main():
     initialize()
